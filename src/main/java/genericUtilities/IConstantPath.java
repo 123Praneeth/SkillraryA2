64b9ce70ab075a194f9588e631eb339f413fc09a
prane <prane@praneeth>
2024-02-03 12:44:59 +0530

    Framework  A2 First Commit

---
+++ src/main/java/genericUtilities/IConstantPath.java
@@ -0,0 +1,13 @@
+package genericUtilities;
+
+/**
+ * This interface contains the paths of external files like properties & excel
+ * @author prane
+ */
+public interface IConstantPath {
+	String PROPERTIES_FILE_PATH = "./src/test/resources/CommonData.properties";
+	String EXCEL_PATH = "./src/test/resources/Selenium.xlsx";
+	
+	
+
+}
